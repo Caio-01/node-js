90281d8b853e3e41cf9b3eb5fb5f358e7bf99798
caio <caiosantos.costa02@gmail.com>
2024-03-21 17:33:41 -0300

    first file about testing

--- 04-tests/01/service.js
+++ 04-tests/01/service.js
@@ -5,14 +5,15 @@ const URL = `https://swapi.dev/api/people`; //URL da API
 async function obterPessoas(nome) {
   const url = `${URL}/?search=${nome}&format=json`;
   const result = await get(url);
-  return result.data.results.map(mapearPessoas) //Mapeando conforme a funcao
+  console.log(result.data);
+  return result.data.results.map(mapearPessoas); //Mapeando conforme a funcao
 }
 
-function mapearPessoas(item){
-    return{
-        nome: item.name,
-        peso: item.height
-    }
+function mapearPessoas(item) {
+  return {
+    nome: item.name,
+    peso: item.height,
+  };
 }
 
 module.exports = {
--- 04-tests/01/tests.js
+++ 04-tests/01/tests.js
@@ -1,14 +1,48 @@
 const assert = require("assert"); //Modulo de assercao
 const { obterPessoas } = require("./service"); //Modulo de service
 
+//Instalando o pacote nock, para simular requisicoes
+const nock = require("nock");
+
 //Para comecar com o Mocha
-describe("Star Wars Tests", () => {
+describe("Star Wars Tests", function () {
+  //Antes de entrar no 'it', ele vai executar essa tarefa:
+  this.beforeAll(() => {
+    const response = {
+      count: 1,
+      next: null,
+      previous: null,
+      results: [
+        {
+          name: "R2-D2",
+          height: "96",
+          mass: "32",
+          hair_color: "n/a",
+          skin_color: "white, blue",
+          eye_color: "red",
+          birth_year: "33BBY",
+          gender: "n/a",
+          homeworld: "https://swapi.dev/api/planets/8/",
+          vehicles: [],
+          starships: [],
+          created: "2014-12-10T15:11:50.376000Z",
+          edited: "2014-12-20T21:17:50.311000Z",
+          url: "https://swapi.dev/api/people/3/",
+        },
+      ],
+    };
+
+    nock("https://swapi.dev/api/people")
+      .get("/?search=r2-d2&format=json")
+      .reply(200, response);
+  });
+
   //Definindo os testes que vao rodar
   it("deve buscar o r2d2 com o formato correto", async () => {
     const expected = [{ nome: "R2-D2", peso: "96" }]; //Definindo o esperado
     const nomeBase = "r2-d2";
 
     const resultado = await obterPessoas(nomeBase);
-    assert.deepEqual(resultado, expected);//Teste
+    assert.deepEqual(resultado, expected); //Teste
   });
 });
