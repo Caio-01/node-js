c9577fb9f784e5f11684be1aeb007fa385966d9e
caio <caiosantos.costa02@gmail.com>
2024-03-26 15:58:14 -0300

    folder update

--- 05-cli/database.js
+++ 05-cli/database.js
@@ -1,29 +1,33 @@
-const { readFile } = require("fs"); //Importando Módulo
+const { readFile } = require("fs"); //Importando a função "readFile" do módulo "fs"
 
-const { promisify } = require("util"); //Importado Módulo para promisse
+const { promisify } = require("util"); //Importando a função "promisify" do módulo "util"
 
-const readFileAsync = promisify(readFile);
+const readFileAsync = promisify(readFile); //Criando uma versão promissificada da função "readFile"
 
-//outra forma de obter dados do JSON
+//Outra forma de obter dados do JSON
 //const dadosJson = require('./herois.json')
 
+//Definindo uma classe chamada "Database"
 class Database {
   constructor() {
-    this.NOME_ARQUIVO = "herois.json";
+    this.NOME_ARQUIVO = "herois.json"; //Definindo o nome do arquivo JSON a ser usado
   }
 
+  //Método para obter dados do arquivo
   async obterDadosArquivos() {
-    const arquivo = await readFileAsync(this.NOME_ARQUIVO, "utf8");
+    const arquivo = await readFileAsync(this.NOME_ARQUIVO, "utf8"); //Obtendo tudo que tem nesse arquivo => NOME_ARQUIVO
     return JSON.parse(arquivo.toString());
   }
 
+
   escreverArquivo() {}
 
+  //Método para listar dados (filtrados por ID, se fornecido)
   async listar(id) {
-    const dados = await this.obterDadosArquivos();
-    const dadosFiltrados = dados.filter((item) => (id ? item.id === id : true));
-    return null;
+    const dados = await this.obterDadosArquivos(); //Obtendo dados do arquivo
+    const dadosFiltrados = dados.filter(item => (id ? ( item.id === id): true)); //Filtrando dados com base no ID fornecido
+    return dadosFiltrados; //Retornando os dados Filtrados
   }
 }
 
-module.exports = new Database(); //exportando a instância
+module.exports = new Database(); //Exportando uma instância da classe "Database"
--- 05-cli/test.js
+++ 05-cli/test.js
@@ -1,4 +1,6 @@
-const { deepEqual, equal } = require("assert"); // Importando metodo deepEqual, equal do assert
+const { deepEqual, ok } = require("assert"); // Importando metodo deepEqual, ok do módulo assert
+
+const dataBase = require("./database"); //Importando o dataBase
 
 const DEFAULT_ITEM_CADASTAR = {
   nome: "Flash",
@@ -9,11 +11,12 @@ const DEFAULT_ITEM_CADASTAR = {
 //Iniciando o conjunto de testes com o Mocha
 describe("Suite de manipulação de herois", () => {
   //Definindo o caso de teste
-  it('deve pesquisar um heroi usando arquivos', async () => {
+  it("deve pesquisar um heroi usando arquivos", async () => {
     const expected = DEFAULT_ITEM_CADASTAR;
+    const [resultado] = await dataBase.listar(expected.id); //Listando o usuário com o id específico
 
-    ok(null, expected)
-  })
+    deepEqual(resultado, expected);
+  });
 
   //   it("deve cadastar um heroi, usando arquivos", async () => {
   //     //Definindo o esperado
